afd0d2ce1b82e92147a6ad219bc4798de10070cd
jthomp888999 <jthomp@protonmail.com>
2017-08-16 11:27:15 -0500

    setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from distutils.core import setup
 
 setup(
-    name = 'Diceware Generator',
+    name = 'Diceware-Generator',
     version = '0.0.1',
     author = 'JoTho',
     author_email = 'jthomp@protonmail.com',
+    url='https://github.com/diceware',
     description = 'Simple diceware python generator')
